699aeeb1b186165c9058d69d3e78beacaef95132
Irakliy <irakliy81@yahoo.com>
2016-09-30 14:14:27 -0700

    updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,42 @@
-var gulp = require('gulp');
-var del = require('del');
-var exec = require('child_process').exec;
+'use strict';
+// IMPORTS
+// ================================================================================================
+const gulp  = require('gulp');
+const del   = require('del');
+const exec  = require('child_process').exec;
 
+// TASKS
+// ================================================================================================
 // delete bin folder
 gulp.task('clean', function(cb) {
-  del(['bin'], cb);
+  del(['bin']).then(() => { cb(); });
 });
 
 // compile TypeScript files
-gulp.task('build', ['clean'], function (cb) {
-    gulp.src('./package.json').pipe(gulp.dest('./bin'));
-    gulp.src('./config/*').pipe(gulp.dest('./bin/config'));
-    gulp.src('./.npmignore').pipe(gulp.dest('./bin'));
-    gulp.src('./README.md').pipe(gulp.dest('./bin'));
-      
-    exec('tsc -p .', function (err, stdout, stderr) {
-        if (stdout.length > 0) console.log(stdout);
-        if (stderr.length > 0) console.error(stderr);
-        cb(err);
-    });
+gulp.task('compile', ['clean'], function (cb) {  
+  exec('tsc -p .', function (err, stdout, stderr) {
+    if (stdout.length > 0) console.log(stdout);
+    if (stderr.length > 0) console.error(stderr);
+    cb(err);
+  });
+});
+
+// build the project
+gulp.task('build', ['compile'], function (cb) {
+  gulp.src('./package.json').pipe(gulp.dest('./bin'));
+  gulp.src('./nova-server.d.ts').pipe(gulp.dest('./bin'));
+  gulp.src('./.settings/.npmignore').pipe(gulp.dest('./bin'));
+  gulp.src('./README.md').pipe(gulp.dest('./bin'));
+  cb();
 });
 
 // publish to npm
 gulp.task('publish', ['build'], function (cb) {
-    exec('npm publish bin --access=public', function (err, stdout, stderr) {
-        if (stdout.length > 0) console.log(stdout);
-        if (stderr.length > 0) console.error(stderr);
-        cb(err);
-    });
+  exec('npm publish bin --access=public', function (err, stdout, stderr) {
+    if (stdout.length > 0) console.log(stdout);
+    if (stderr.length > 0) console.error(stderr);
+    cb(err);
+  });
 });
 
 // define default task
